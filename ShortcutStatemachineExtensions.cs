7f59d232e9ba07f63372d225a093af842bf0d7d7
danielmarbach <daniel.marbach@openplace.net>
2019-09-18 16:27:55 +0200

    RC1

--- ShortcutStatemachineExtensions.cs
+++ ShortcutStatemachineExtensions.cs
@@ -29,21 +29,21 @@ static class ShortcutStatemachineExtensions
   - The code is easier to modify (consider adding a using, for example).
   - Diagnostics of asynchronous methods are easier (debugging hangs etc).
   - Exceptions thrown will be automatically wrapped in the returned Task instead of surprising the caller with an actual exception.
-- NET Core 2.0:
+- NET Core 2.2:
 
- |  Method |      Mean |     Error |    StdDev | Scaled | Allocated |
- |-------- |----------:|----------:|----------:|-------:|----------:|
- |  Return | 15.576 ms | 0.4185 ms | 0.0236 ms |   1.00 |     528 B |
- |  Simple | 15.568 ms | 0.8275 ms | 0.0468 ms |   1.00 |     744 B |
- | Actions |  2.008 ms | 0.0756 ms | 0.0043 ms |   0.13 |     560 B |
+|  Method | Allocated |
+|-------- |----------:|
+|  Return |     376 B |
+|  Await  |     488 B |
+| Actions |     352 B |
 
-- NET Core 2.1:
+- NET Core 3.0 RC1:
 
- |  Method |      Mean |     Error |    StdDev | Scaled | Allocated |
- |-------- |----------:|----------:|----------:|-------:|----------:|
- |  Return | 15.542 ms | 1.3313 ms | 0.0752 ms |   1.00 |     376 B |
- |  Simple | 15.538 ms | 1.4433 ms | 0.0815 ms |   1.00 |     488 B |
- | Actions |  1.939 ms | 0.4590 ms | 0.0259 ms |   0.12 |     350 B |
+|  Method | Allocated |
+|-------- |----------:|
+|  Return |     344 B |
+|  Await  |     456 B |
+| Actions |     348 B |
 
 ");
     }
