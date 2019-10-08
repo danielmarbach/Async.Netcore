2617d147094d065e4d86817ffa3e1997129a0546
danielmarbach <daniel.marbach@openplace.net>
2019-10-08 11:39:44 +0200

    Less examples

--- RunnableProvider.cs
+++ RunnableProvider.cs
@@ -2,5 +2,5 @@ static class RunnableProvider
 {
     public static IRunnable[] All() => new IRunnable[] {
         new SyncOverAsync(), new GoodCitizen(), new UnobservedException(), new CompletedTask(), new ShortcutStatemachine(), new StackTracesOhMy(),
-        new ValueTasks(), new AsyncDisposable(), new AsyncEnumerable(), new DefaultInterfaces(), new HostedServices(), new Pipelines(), new Channels() };
+        new ValueTasks(), new AsyncDisposable(), new AsyncEnumerable(), new DefaultInterfaces() };
 }
